dae3c1efe69a706c9ab922f9104f4f3e5b1ab16d
Abdel! <32202417+Abdel-2020@users.noreply.github.com>
2022-07-11 10:03:37 +0100

    Commented JS Code

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// Get input from input field
-// 1st store the input field into a variable. 
-//Store the input button into a variable. 
-//Store the Task List into a variable
 
+// 1st store the input field into a variable. 
 let inputTaskField = document.getElementById("todo-input");
+
+//Store the input button into a variable. 
 let inputSubmitBtn = document.getElementById("todo-submit");
+
+//Store the Task List into a variable
 let taskList = document.querySelector(".task-list");
 
 
 
 //Grabs text from input field and returns the value. 
 const readInput = () => {
+    //Event listener waits for a click on the inputSubmitButton
     inputSubmitBtn.addEventListener("click", () => {
-
+        //logs to console to test if it the click functionality works
         console.log(`${inputTaskField.value}`);
+        //Runs the createListElement function, passing the value of what was typed in the inputTaskField.
         createListElement(inputTaskField.value);
     })
-    return inputTaskField.value;
 }
 
 
@@ -27,12 +29,16 @@ const readInput = () => {
 
 const createListElement = (inputTaskFieldValue) => {
   
+    //Creates the li element. 
      const li = document.createElement("li");
+    //Creates the p element.
      const p = document.createElement("p");
-
-    p.innerHTML= inputTaskFieldValue;
+    //Stores the value passed from readInput() inside the p element. 
+    p.innerHTML = inputTaskFieldValue;
+    //Nests the p element inside the li element.
     li.appendChild(p);
 
+    //Calls the appendBtn() function to nest a button element into the li Element.
     appendBtn(li);
     taskList.appendChild(li);
 
@@ -46,11 +52,12 @@ const appendBtn = (listItem) => {
     //Creates Button
     let btn = document.createElement("input");
 
+    //After button is created we need to add its attributes, Type="Button", id="task-submit", value="✔️"
     btn.setAttribute("type", "button");
     btn.setAttribute("id", "task-submit");
     btn.setAttribute("value", "✔️");
+    //add class  .button
     btn.classList.add("button");
-
 
     //Append Button
     listItem.appendChild(btn);
