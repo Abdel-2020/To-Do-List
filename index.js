e01ab840129edc9954e0e3d7248c466909077445
Abdel-2020 <abdelnoura97@gmail.com>
2022-07-13 21:25:36 +0100

    Cleaned up code

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// 1st store the input field into a variable. 
-let inputTaskField = document.getElementById("todo-input");
-
-//Store the input button into a variable. 
-let inputSubmitBtn = document.getElementById("todo-submit");
-
-//Store the Task List into a variable
-let taskList = document.querySelector(".task-list");
-
-//Store the task Submit button into a variable
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 //Read input function----------------------------------------------------------------------------------------------------------------------------------------------------------
 //Grabs text from input field and returns the value.  
 const readInput = () => {
+    // 1st store the input field into a variable. 
+    let inputTaskField = document.getElementById("todo-input");
+    //Store the input button into a variable. 
+    let inputSubmitBtn = document.getElementById("todo-submit");
+
     //Event listener waits for a click on the inputSubmitButton
     inputSubmitBtn.addEventListener("click", () => {
         //logs to console to test if it the click functionality works
@@ -26,6 +17,7 @@ const readInput = () => {
         } else {
             //Runs the createListElement function, passing the value of what was typed in the inputTaskField.
             createListElement(inputTaskField.value);
+
         }
     })
 }
@@ -35,6 +27,8 @@ const readInput = () => {
 //Create List Element function//Read input function----------------------------------------------------------------------------------------------------------------------------------
 //Creates List Element, nest P element and add the input text
 const createListElement = (inputTaskFieldValue) => {
+//Store the Task List into a variable
+let taskList = document.querySelector(".task-list");
 
     //Creates the li element. 
     const li = document.createElement("li");
@@ -56,22 +50,31 @@ const createListElement = (inputTaskFieldValue) => {
 //Append button  Function----------------------------------------------------------------------------------------------------------------------------------------------------------
 const appendBtn = (listItem) => {
     //Creates Button
-    let btn = document.createElement("input");
+    let btn = document.createElement("button");
 
     //After button is created we need to add its attributes, Type="Button", id="task-submit", value="✔️"
     btn.setAttribute("type", "button");
     btn.setAttribute("id", "task-submit");
-    btn.setAttribute("value", "✔️");
+    btn.innerHTML = "✔️";
     //add class  .button
     btn.classList.add("button");
 
     //Append Button
     listItem.appendChild(btn);
+
+
 }
 //End of Append button  Function----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 //Task Submit function-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+//Read if the tick button has been pressed. 
+
+
+
 
 /*
 const taskSubmit = () => {
@@ -84,8 +87,9 @@ const taskSubmit = () => {
     })
 }
 
+    
 
-
+*/
 
 
 
